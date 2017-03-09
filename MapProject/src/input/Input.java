69b3cac397984d884c02629a5305ab6c6a09f756
PurifyPioneer <klook.christian@gmail.com>
2017-03-09 13:23:11 +0100

    Added documentation for input handling.

--- MapProject/src/input/Input.java
+++ MapProject/src/input/Input.java
@@ -1,25 +1,50 @@
 package input;
 
+/**
+ * Class that holds general information about the input.
+ * This gives us the possibility to easily implement
+ * some sort of controller or other sorts of input.
+ * @author PurifyPioneer
+ * @version 1.0
+ * @since 1.0
+ */
 public class Input {
 
+	// TODO maybe make further abstraction because currently this
+	// is focused only on the player.
+	// This is left here on purpose because the current version is enough for our needs.
+	
 	private boolean isJumping;
 	private boolean isDucking;
 	
-	public Input() {
-	}
-
+	/**
+	 * Set the player to jumping. (Request a jump. Actual logic is handler by GameObjectHandler)
+	 * @param isJumping
+	 */
 	public void setJumping(boolean isJumping) {
 		this.isJumping = isJumping;
 	}
 	
+	/**
+	 * Returns if the player is currently jumping/wants to jump.
+	 * @return
+	 */
 	public boolean isJumping() {
 		return this.isJumping;
 	}
 	
+	/**
+	 * Set the player to ducking. (Request a duck. Actual logic is handler by GameObjectHandler)
+	 * @param isDucking
+	 */
 	public void setDucking(boolean isDucking) {
 		this.isDucking = isDucking;
 	}
 	
+	/**
+	 * Returns if the player is currently ducking/wants to duck.
+	 * @return
+	 */
 	public boolean isDucking() {
 		return this.isDucking;
 	}
--- MapProject/src/input/KeyInput.java
+++ MapProject/src/input/KeyInput.java
@@ -5,44 +5,50 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 import actionlogger.ActionLogger;
-import core.SoundHandler;
-import gameobjects.GameObjectHandler;
-import gameobjects.Player;
+import game.RunningBronto;
 
+/**
+ * Class the specifically handles the keyboard input.
+ * It notifies the input class on what the user wants to do.
+ * From there the object handler gets its information.
+ * @author PurifyPioneer
+ * @version 1.0
+ * @since 1.0
+ */
 public class KeyInput extends Input implements KeyListener{
-	
-	public KeyInput() {
-		super();
-	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
+		// player jump
 		if (e.getKeyCode() == KeyEvent.VK_SPACE) {
 			ActionLogger.LogAction("PLAYER JUMPING");
 			setJumping(true);
-			SoundHandler.playJump();
 		}
+		// player duck
 		if (e.getKeyCode() == KeyEvent.VK_SHIFT) {
+			ActionLogger.LogAction("PLAYER DUCKING");
 			setDucking(true);
 		}
-
-//		// pausing the game
-//		if (e.getKeyCode() == KeyEvent.VK_P) {
-//			if (paused) {
-//				ActionLogger.LogAction("Game unpaused!", Color.ORANGE);
-//			} else {
-//				ActionLogger.LogAction("Game paused!", Color.RED);
-//			}
-//			paused = !paused;
-//		}
+		// pausing the game
+		if (e.getKeyCode() == KeyEvent.VK_P) {
+			if (RunningBronto.isPaused()) {
+				ActionLogger.LogAction("Game unpaused!", Color.ORANGE);
+			} else {
+				ActionLogger.LogAction("Game paused!", Color.RED);
+			}
+			// toggle paused state
+			RunningBronto.setPaused(!RunningBronto.isPaused());;
+		}
 
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
+		// player not jumping any longer
 		if (e.getKeyCode() == KeyEvent.VK_SPACE) {
 			setJumping(false);
 		}
+		// player not ducking any longer
 		if (e.getKeyCode() == KeyEvent.VK_SHIFT) {
 			setDucking(false);
 		}
@@ -50,7 +56,5 @@ public class KeyInput extends Input implements KeyListener{
 
 	@Override
 	public void keyTyped(KeyEvent arg0) {
-
 	}
-	
 }
